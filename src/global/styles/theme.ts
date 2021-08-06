48530261838a0d82f565ee3e44bb8c36c43c8168
Júnior Nunes <denisonprobable13@gmail.com>
2021-08-05 23:30:53 -0300

    Global theme created

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    background_primary: '#24006A',
+    background_secondary: '#E3E1E9',
+
+    shape: '#FFFFFF',
+
+    Title: '#42377F',
+    subtitle: '#6180CF',
+    text: '#6D6D6D',
+
+    success: '#008000',
+    main: '#E00026'
+  },
+  fonts: {
+    primary_regular: 'NotoSans_400Regular',
+    primary_bold: 'NotoSans_700Bold',
+    secondary: 'Roboto_400Regular'
+  }
+};
