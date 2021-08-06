d10a0a9f49b5b40408202f30bd515bce35f58921
Júnior Nunes <denisonprobable13@gmail.com>
2021-08-05 23:31:16 -0300

    using global theme and google fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto';
+
+import {
+  NotoSans_400Regular,
+  NotoSans_700Bold
+} from '@expo-google-fonts/noto-sans';
+
+import theme from './src/global/styles/theme';
 import { Home } from './src/screens/Home';
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    NotoSans_400Regular,
+    NotoSans_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
