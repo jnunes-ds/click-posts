c3f9842786ea93ab53f3c00069c4d433bd8e2f1c
Júnior Nunes <denisonprobable13@gmail.com>
2021-08-07 02:21:28 -0300

    Profile screen prototype created

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { Header } from '../../components';
 
-import { Container, Content } from './styles';
+import {
+  Container,
+  Content,
+  Body,
+  InformationsContainer,
+  UserInformation,
+  UserLabel,
+  UserInfo,
+  CompanyInformationContainer,
+  CompanyTitle,
+  CompanyInformation,
+  CompanyInfoLabel,
+  CompanyInfo,
+} from './styles';
 
 export function Profile() {
   return (
@@ -14,6 +27,39 @@ export function Profile() {
       />
       <Content>
         <Header userName="Breth" name="Leane Graham" />
+        <Body>
+          <InformationsContainer>
+            <UserInformation>
+              <UserLabel>Email</UserLabel>
+              <UserInfo>incere@april.bi</UserInfo>
+            </UserInformation>
+            <UserInformation>
+              <UserLabel>WebSite</UserLabel>
+              <UserInfo>hildegard.org</UserInfo>
+            </UserInformation>
+            <UserInformation>
+              <UserLabel>Phone</UserLabel>
+              <UserInfo>1-770-736-8031 x56442</UserInfo>
+            </UserInformation>
+            <CompanyInformationContainer>
+              <CompanyTitle>Company</CompanyTitle>
+              <CompanyInformation>
+                <CompanyInfoLabel>Name:</CompanyInfoLabel>
+                <CompanyInfo>Romaguera-Crona</CompanyInfo>
+              </CompanyInformation>
+              <CompanyInformation>
+                <CompanyInfoLabel>Catch Phrase:</CompanyInfoLabel>
+                <CompanyInfo>
+                  Multi-layered client-server neural-net
+                </CompanyInfo>
+              </CompanyInformation>
+              <CompanyInformation>
+                <CompanyInfoLabel>BS:</CompanyInfoLabel>
+                <CompanyInfo>harness real-time e-markets</CompanyInfo>
+              </CompanyInformation>
+            </CompanyInformationContainer>
+          </InformationsContainer>
+        </Body>
       </Content>
     </Container>
   );
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -14,3 +14,53 @@ export const Content = styled.View`
   border-radius: ${RFValue(55)}px;
   overflow: hidden;
 `;
+
+export const Body = styled.View`
+  flex: 1;
+  padding: ${RFValue(10)}px;
+`;
+
+export const InformationsContainer = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.shape};
+  border-radius: 10px;
+  padding: ${RFValue(43)}px;
+`;
+
+export const UserInformation = styled.View`
+  margin-bottom: 15px;
+`;
+
+export const UserLabel = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_bold};
+  font-size: ${RFValue(22)}px;
+  color: ${({ theme }) => theme.colors.Title};
+`;
+
+export const UserInfo = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary};
+  font-size: ${RFValue(22)}px;
+  color: ${({ theme }) => theme.colors.subtitle};
+`;
+
+export const CompanyInformationContainer = styled.View``;
+
+export const CompanyTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_bold};
+  font-size: ${RFValue(26)}px;
+  color: ${({ theme }) => theme.colors.Title};
+`;
+
+export const CompanyInformation = styled.View``;
+
+export const CompanyInfoLabel = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_bold};
+  font-size: ${RFValue(22)}px;
+  color: ${({ theme }) => theme.colors.subtitle};
+`;
+
+export const CompanyInfo = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary};
+  font-size: ${RFValue(18)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
